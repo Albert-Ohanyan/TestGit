aeb1c96b0183c14c4db21239a59034f9b3afae2d
Albert-Ohanyan <z.albert.ohanyan@gmail.com>
2020-02-02 08:32:51 -0800

    U1C006

--- src/main/java/com/git/main/Test.java
+++ src/main/java/com/git/main/Test.java
@@ -3,6 +3,6 @@ package com.git.main;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("GIT001444ppppfsdf sdf");
+        System.out.println("GIT001444ppppfsdf gddffgsdf");
     }
 }
